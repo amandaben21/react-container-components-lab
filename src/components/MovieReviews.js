8a117e88a29763550248d09b4acdfb93962db7f3
Amanda <amandaben21@aol.com>
2021-07-10 21:30:32 -0400

    commit tree bc50081210a0330356d621114677f3798e39b0d4

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,11 @@
-// Code MovieReviews Here
+import React from 'react';
+
+const MovieReview = (props) => {
+    return (
+        <div className="review-list">
+            { props.reviews.map(review => <p className="review">review</p>) }
+        </div>
+    )
+}
+
+export default MovieReview;
